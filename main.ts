1592b99616e81984a8c1d3689b32999cf19231e2
ansango <anibalsantosgo@gmail.com>
2024-05-26 21:48:00 +0200

    first commit

--- main.ts
+++ main.ts
@@ -1,85 +1,85 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
+import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, Setting, requestUrl } from 'obsidian';
+import metadata from "metadata-scraper";
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface MetadataLinksSettings {
+	render: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: MetadataLinksSettings = {
+	render: 'html',
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class MetadataLinks extends Plugin {
+	settings: MetadataLinksSettings;
 
 	async onload() {
 		await this.loadSettings();
-
+		this.settings
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
+		this.addRibbonIcon('dice', 'Metadata links', (evt: MouseEvent) => {
+
 			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+			const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+			if (markdownView) {
+				const editor = markdownView.editor;
+				this.mapUrls(editor, this.settings);
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
+
+
 		this.addCommand({
 			id: 'sample-editor-command',
 			name: 'Sample editor command',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				this.mapUrls(editor, this.settings);
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new MetadataLinksSettingTab(this.app, this));
 	}
 
 	onunload() {
+		console.log("unloading plugin");
+	}
 
+	isValidURL(url: string): boolean {
+		const urlRegex = /^(https?:\/\/)?([a-zA-Z\d-]+\.)+[a-zA-Z]{2,6}(\/[\w .-]*)*(\?[\w=&%.-]*)?(#[\w-]*)?$/;
+		return urlRegex.test(url);
+	}
+
+	getUrls(text: string): string[] {
+		const urls = text.match(/(https?:\/\/[^\s]+)/g) || [];
+		return urls.filter(url => this.isValidURL(url));
+	}
+
+
+
+	async mapUrls(editor: Editor, { render }: MetadataLinksSettings): Promise<void> {
+		const text = editor.somethingSelected()
+			? editor.getSelection().trim()
+			: false;
+
+		if (text) {
+			const urls = this.getUrls(text)
+			try {
+				const results = await Promise.allSettled(urls.map(url => requestUrl(url).then(({ text }) => ({ html: text, url })))).then(results => results.map(result => result.status === 'fulfilled' ? result.value : result.reason));
+				const data = await Promise.allSettled(results.map(({ html, url }) => metadata({ html, url }).then(({ title, description, icon, image, url }) => ({ title, description, icon, image, url })))).then(results => results.map(result => result.status === 'fulfilled' ? result.value : result.reason));
+				new Notice(`Found ${data.length} metadata links`);
+				const previousSelection = text + "\n\n";
+				const plain = data.map(({ title, description, url }) => `- [${title}](${url}): ${description}`).join("\n\n");
+				const html = data.map(({ title, description, image, url }) => `<div class=link-card-container><a class=link-card href=${url} target=_blank><div class=link-image-container><div class=link-image style=background-image:url(${image})></div></div><div class=link-card-text><h5 class=link-card-title>${(title || "").replace(/\s{3,}/g, ' ').trim()}</h5><p class=link-card-description>${(description || "").replace(/\s{3,}/g, ' ').trim()}<p class=link-href>${url}</div></a></div>`).join("\n\n");
+				editor.replaceSelection(previousSelection + (render === 'html' ? html : plain) + "\n\n");
+				new Notice("Converted URLs to metadata links");
+			} catch (error) {
+				new Notice("Error converting URLs to metadata links");
+			}
+
+		} else {
+			new Notice("Select almost a URL to convert");
+		}
 	}
 
 	async loadSettings() {
@@ -91,44 +91,30 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+class MetadataLinksSettingTab extends PluginSettingTab {
+	plugin: MetadataLinks;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: MetadataLinks) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Choose how to render metadata links')
+			.setDesc('By default, metadata links are rendered as HTML')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('html || markdown')
+				.setValue(this.plugin.settings.render)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.render = value;
 					await this.plugin.saveSettings();
 				}));
+
 	}
 }
